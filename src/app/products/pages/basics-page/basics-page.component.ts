3bd9e6ba917fa5b75cc95827c8a40a8e0f28a7ac
Juan-CT <ct.juan.88@gmail.com>
2024-09-23 19:36:14 +0200

    Avanzando en la sección de pipes

---
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basics-page',
+  templateUrl: './basics-page.component.html',
+  styleUrl: './basics-page.component.css'
+})
+
+export class BasicsPageComponent {
+
+  public nameLower: string = 'pepe';
+  public nameUpper: string = 'PEPE';
+  public fullName: string = 'pEPe ViyuELa';
+
+  public customDate: Date = new Date();
+}
